f268d0dd197fba1a6305063d4621c8a012809c82
Denis Sholkovy <denissholkovy@gmail.com>
2024-01-24 12:20:01 +0200

    fix style and css

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ let c = console.log;
 
 // random
 
-let rombRed = document.querySelector(".circle-red");
-let rombBlue = document.querySelector(".circle-blue");
+let rombRed = document.querySelector('.circle-red');
+let rombBlue = document.querySelector('.circle-blue');
+let rombGreen = document.querySelector('.circle-green');
+
 
 setInterval(() => {
   let randomX = Math.floor(Math.random() * 100);
@@ -13,7 +15,8 @@ setInterval(() => {
 
   rombRed.style.top = `${randomY}%`;
   rombRed.style.left = `${randomX}%`;
-}, 8000);
+},5000)
+
 
 setInterval(() => {
   let randomX = Math.floor(Math.random() * 100);
@@ -21,33 +24,77 @@ setInterval(() => {
 
   rombBlue.style.top = `${randomY}%`;
   rombBlue.style.left = `${randomX}%`;
-}, 7000);
+},6000)
 
-//random number
+setInterval(() => {
+  let randomX = Math.floor(Math.random() * 100);
+  let randomY = Math.floor(Math.random() * 100);
 
-let val = document.querySelector(".randon-text");
-let btnRandom = document.querySelector(".btn-random");
+  rombGreen.style.top = `${randomY}%`;
+  rombGreen.style.left = `${randomX}%`;
+},7000)
 
-btnRandom.onclick = function () {
-  let randomNum = Math.floor(Math.random() * 5);
 
-  // с(randomNum)
 
-  switch (randomNum) {
-    case 1:
-      val.innerHTML = `${randomNum}. сделать слайдер`;
-      break;
-    case 2:
-      val.innerHTML = `${randomNum}. сделать адаптив`;
-      break;
-    case 3:
-      val.innerHTML = `${randomNum}. сделать анимацию`;
-      break;
-    case 4:
-      val.innerHTML = `${randomNum}. сделать чеклист`;
-      break;
-    case 5:
-      val.innerHTML = `${randomNum}. сделать валидацию`;
-      break;
-  }
-};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // //random number
+
+// let val = document.querySelector(".randon-text");
+// let btnRandom = document.querySelector(".btn-random");
+
+// btnRandom.onclick = function () {
+//   let randomNum = Math.floor(Math.random() * 5);
+
+//   // с(randomNum)
+
+//   switch (randomNum) {
+//     case 1:
+//       val.innerHTML = `${randomNum}. сделать слайдер`;
+//       break;
+//     case 2:
+//       val.innerHTML = `${randomNum}. сделать адаптив`;
+//       break;
+//     case 3:
+//       val.innerHTML = `${randomNum}. сделать анимацию`;
+//       break;
+//     case 4:
+//       val.innerHTML = `${randomNum}. сделать чеклист`;
+//       break;
+//     case 5:
+//       val.innerHTML = `${randomNum}. сделать валидацию`;
+//       break;
+//   }
+// };

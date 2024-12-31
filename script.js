6c2acd1b26cf8805f840a8fa52c78267baa1069b
Denis Sholkovy <denissholkovy@gmail.com>
2024-12-31 17:18:53 +0200

    update new code

--- script.js
+++ script.js
@@ -4,97 +4,38 @@ let c = console.log;
 
 // random
 
-let rombRed = document.querySelector('.circle-red');
-let rombBlue = document.querySelector('.circle-blue');
-let rombGreen = document.querySelector('.circle-green');
+class Random {
+  constructor({ time, blCircle }) {
+    this.time = time;
+    this.blCircle = blCircle;
 
+    this.random();
+  }
 
-setInterval(() => {
-  let randomX = Math.floor(Math.random() * 100);
-  let randomY = Math.floor(Math.random() * 100);
+  random() {
+    setInterval(() => {
+      let randomX = Math.floor(Math.random() * 100);
+      let randomY = Math.floor(Math.random() * 100);
 
-  rombRed.style.top = `${randomY}%`;
-  rombRed.style.left = `${randomX}%`;
-},5000)
+      this.blCircle.style.top = `${randomY}%`;
+      this.blCircle.style.left = `${randomX}%`;
+      
+    }, this.time);
+  }
+}
 
+new Random({
+  time: 5000,
+  blCircle: document.querySelector(".circle-red"),
+});
 
-setInterval(() => {
-  let randomX = Math.floor(Math.random() * 100);
-  let randomY = Math.floor(Math.random() * 100);
+new Random({
+  time: 6000,
+  blCircle: document.querySelector(".circle-blue"),
+});
 
-  rombBlue.style.top = `${randomY}%`;
-  rombBlue.style.left = `${randomX}%`;
-},6000)
+new Random({
+  time: 7000,
+  blCircle: document.querySelector(".circle-green"),
+});
 
-setInterval(() => {
-  let randomX = Math.floor(Math.random() * 100);
-  let randomY = Math.floor(Math.random() * 100);
-
-  rombGreen.style.top = `${randomY}%`;
-  rombGreen.style.left = `${randomX}%`;
-},7000)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // //random number
-
-// let val = document.querySelector(".randon-text");
-// let btnRandom = document.querySelector(".btn-random");
-
-// btnRandom.onclick = function () {
-//   let randomNum = Math.floor(Math.random() * 5);
-
-//   // с(randomNum)
-
-//   switch (randomNum) {
-//     case 1:
-//       val.innerHTML = `${randomNum}. сделать слайдер`;
-//       break;
-//     case 2:
-//       val.innerHTML = `${randomNum}. сделать адаптив`;
-//       break;
-//     case 3:
-//       val.innerHTML = `${randomNum}. сделать анимацию`;
-//       break;
-//     case 4:
-//       val.innerHTML = `${randomNum}. сделать чеклист`;
-//       break;
-//     case 5:
-//       val.innerHTML = `${randomNum}. сделать валидацию`;
-//       break;
-//   }
-// };
